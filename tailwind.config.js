cd3f1d3892073696393cc2ed27c8617a32ee2cda
Divyanshu Singh <dsinecos@gmail.com>
2020-05-21 16:53:53 +0530

    add styling for loop 1 - step 7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   theme: {
     extend: {
       width: {
         '96': '24rem'
+      },
+      screens: {
+        's-w-400': '400px'
       }
     },
   },
   variants: {
-    borderWidth: ['responsive', 'hover']
+    borderWidth: ['responsive', 'hover'],
+    fontSize: ['responsive', 'first-letter'],
+    fontWeight: ['responsive', 'first-letter'],
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addVariant, e }) {
+      addVariant('first-letter', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`first-letter${separator}${className}`)}::first-letter`
+        })
+      })
+    })
+  ],
 }
